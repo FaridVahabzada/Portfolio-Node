fd2baf7d5b07e723f6cf62e354e1f3e3674b23fb
Farid Vahabzada <faridvahabzada@gmail.com>
2024-01-22 16:23:18 +0100

    Add typed.js

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(__dirname + '/node_modules/bootstrap/dist')); //added in Project 1 in "Restructure - Dependencies" lesson
 app.use(express.static(__dirname + '/node_modules/jquery/dist/')); //added in Project 1 in "Restructure - Dependencies" lesson
+app.use(express.static(__dirname + '/node_modules/typed.js/lib')); //added in Project 1 in "Restructure - Dependencies" lesson
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
