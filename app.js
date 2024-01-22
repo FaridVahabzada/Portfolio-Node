232dac53ada279dcbe37099dc189488f397deb50
Farid Vahabzada <faridvahabzada@gmail.com>
2024-01-22 15:59:53 +0100

    Add Bootstrap

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/node_modules/bootstrap/dist')); //added in Project 1 in "Restructure - Dependencies" lesson
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -37,5 +38,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
