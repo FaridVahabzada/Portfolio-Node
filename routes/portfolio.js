633a3a73799cc585619d08e7b2ee7950e702d520
Farid Vahabzada <faridvahabzada@gmail.com>
2024-01-23 17:26:15 +0100

    Add POST handler for portfolio

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+var request = require('request');
+var bodyparser = require('body-parser');
+var jsonParser = bodyparser.json();
+
 const fs = require('fs');
 const path = require('path');
 
@@ -8,6 +12,26 @@ const path = require('path');
 router.get('/', function(req, res, next) {
   let data = fs.readFileSync(path.resolve(__dirname, "../data/portfolio.json"));
   res.render('portfolio', { cakes: JSON.parse(data)});
+});
+
+//download image to the server:
+var download = function(url, filename, callback){
+  request.head(url, function(err, res, body){
+    request(url).pipe(fs.createWriteStream(path.resolve(__dirname, '../data/img/'+ filename))).on('close', callback);
+  });
+};
+
+router.post('/', jsonParser, function(req, res, next) {
+  let rawdata = fs.readFileSync(path.resolve(__dirname, "../data/portfolio.json"));
+  let portfoliosArray = JSON.parse(rawdata);
+  if(portfoliosArray.filter(x => x.name === req.body.name).length == 0) {
+    download(req.body.url, req.body.name, function(){
+      console.log('done');
+    });
+    const newArray = portfoliosArray.concat([req.body])
+    fs.writeFileSync(path.resolve(__dirname, "../data/portfolio.json"), JSON.stringify(newArray));
+  }
+  res.end();
 });
 
 module.exports = router;
